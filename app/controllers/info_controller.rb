9f1b60f491bd6d9a9f883eb9bf3c14a921ab6d6e
m-ahsen <mehmet.ahsen@hotmail.com.tr>
2013-03-29 19:57:49 +0200

    new design with twitter bootstrap

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,20 @@
+class InfoController < ApplicationController
+  def index
+    @mem_info = %x(free -m).split("\n")[1].split(" ")[1..3]
+    @sys_info = %x(uptime).split(",")
+    @cpu_info = []
+    number_of_cores = %x(cat /proc/cpuinfo).scan(/processor/).size
+    @uptime = @sys_info[0]
+    @cpu_info << @sys_info[2].split(" ")[2].to_f * 100 / number_of_cores
+    @cpu_info << @sys_info[3].split(" ")[0].to_f * 100 / number_of_cores
+    @cpu_info << @sys_info[4].split(" ")[0].to_f * 100 / number_of_cores
+    filesystems = %x(df -h).split("\n")
+    @fs = []
+    filesystems.each do |i|
+      if i.include? "/dev/"
+        @fs << i.split(" ")
+      end
+    end
+    render 'index'
+  end
+end
