2d7cfc96f0057c1fe61f52a55bab749488d2a4f3
m-ahsen <mehmet.ahsen@hotmail.com.tr>
2013-03-29 16:15:23 +0200

    cpu load improvement

--- app/controllers/getinfo_controller.rb
+++ app/controllers/getinfo_controller.rb
@@ -1,18 +1,13 @@
 class GetinfoController < ApplicationController
   def index
     @mem_info = %x(free -m).split("\n")[1].split(" ")[1..3]
-    @sys_info = %x(uptime).split(" ")
+    @sys_info = %x(uptime).split(",")
     @cpu_info = []
+    number_of_cores = %x(cat /proc/cpuinfo).scan(/processor/).size
     @uptime = @sys_info[0]
-    if @uptime.include? "day"
-      @cpu_info << @sys_info[9].split(",")[0]
-      @cpu_info << @sys_info[10].split(",")[0]
-      @cpu_info << @sys_info[11]
-    else
-      @cpu_info << @sys_info[7].split(",")[0]
-      @cpu_info << @sys_info[8].split(",")[0]
-      @cpu_info << @sys_info[9]
-    end
+    @cpu_info << @sys_info[2].split(" ")[2].to_f * 100 / number_of_cores
+    @cpu_info << @sys_info[3].split(" ")[0].to_f * 100 / number_of_cores
+    @cpu_info << @sys_info[4].split(" ")[0].to_f * 100 / number_of_cores
     filesystems = %x(df -h).split("\n")
     @fs = []
     filesystems.each do |i|
